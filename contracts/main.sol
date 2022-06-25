04ee16aaa5b24cd2cddf74366791aae6191c34de
Your Name <ayoube.eddakhly@gmail.com>
2022-06-25 08:39:54 -0700

    escrow contract

--- contracts/main.sol
+++ contracts/main.sol
@@ -1,31 +1,241 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Escrow {
-    enum State { AWAITING_PAYMENT, AWAITING_DELIVERY, COMPLETE }
-    
-    State public currState;
-    int public milestone;
-    address public buyer;
-    address payable public seller;
+import "./SafeMath.sol";
 
-    constructor(address _buyer, address payable _seller) public {
-        buyer = _buyer;
-        seller = _seller;
+
+contract Factory {
+    
+    address[] public allEscrowContracts;
+    uint256 public escrowCount;
+    address public factoryOwner;
+    
+    constructor() public {
+        factoryOwner = msg.sender;
+        escrowCount = 0;
     }
+    
+    function createContract() public {
+        address newContract = address(new Escrow(factoryOwner, escrowCount++));
+        allEscrowContracts.push(newContract);
+    }
+    
+    function getAllContracts() public view returns (address[] memory) {
+        return allEscrowContracts;
+    }
+    
+    function getByID(uint256 queryID) public view returns (address) {
+        return allEscrowContracts[queryID];
+    }
+}
+    
+
+contract Escrow {
+    mapping (address => uint256) private balances;
+
+    address public seller;
+    address public buyer;
+    address public escrowOwner;
+    uint256 public blockNumber;
+    uint public feePercent;
+    uint public escrowID;
+    uint256 public escrowCharge;
+
+    bool public sellerApproval;
+    bool public buyerApproval;
+    
+    bool public sellerCancel;
+    bool public buyerCancel;
+    
+    uint256[] public deposits;
+    uint256 public feeAmount;
+    uint256 public sellerAmount;
+
+    enum EscrowState { unInitialized, initialized, buyerDeposited, serviceApproved, escrowComplete, escrowCancelled }
+    EscrowState public eState = EscrowState.unInitialized;
+
+    event Deposit(address depositor, uint256 deposited);
+    event ServicePayment(uint256 blockNo, uint256 contractBalance);
 
     modifier onlyBuyer() {
-        require(msg.sender == buyer, "should be a buyer to call this method");
-        _;
+        if (msg.sender == buyer) {
+            _;
+        } else {
+            revert();
+        }
     }
 
-    function deposit() onlyBuyer external payable {
-        require(currState == State.AWAITING_PAYMENT, "applicant already paid or did not apply");
-        currState = State.AWAITING_DELIVERY;
+    modifier onlyEscrowOwner() {
+        if (msg.sender == escrowOwner) {
+            _;
+        } else {
+            revert();
+        }
+    }    
+
+    modifier checkBlockNumber() {
+        if (blockNumber > block.number) {
+            _;
+        } else {
+            revert();
+        }
     }
 
-    function confirmMilestone() onlyBuyer external {
-        require(currState == State.AWAITING_DELIVERY, "No delivery in progress");
-        seller.transfer(address(this).balance);
-        currState = State.COMPLETE;
+    modifier ifApprovedOrCancelled() {
+        if ((eState == EscrowState.serviceApproved) || (eState == EscrowState.escrowCancelled)) {
+            _;
+        } else {
+            revert();
+        }
+    }
+
+    constructor(address fOwner, uint256 _escrowID) public {
+        escrowOwner = fOwner;
+        escrowID = _escrowID;
+        escrowCharge = 0;
+    }
+
+    fallback () external { // solhint-disable-line
+        // fallback function to disallow any other deposits to the contract
+        revert();
+    }
+
+    function initEscrow(address _seller, address _buyer, uint _feePercent, uint256 _blockNum) public onlyEscrowOwner {
+        require((_seller != msg.sender) && (_buyer != msg.sender));
+        escrowID += 1;
+        seller = _seller;
+        buyer = _buyer;
+        feePercent = _feePercent;
+        blockNumber = _blockNum;
+        eState = EscrowState.initialized;
+
+        balances[seller] = 0;
+        balances[buyer] = 0;
+    }
+
+    function depositToEscrow() public payable checkBlockNumber onlyBuyer {
+        balances[buyer] = SafeMath.add(balances[buyer], msg.value);
+        deposits.push(msg.value);
+        escrowCharge += msg.value;
+        eState = EscrowState.buyerDeposited;
+        emit Deposit(msg.sender, msg.value); // solhint-disable-line
+    }
+
+    function approveEscrow() public {
+        if (msg.sender == seller) {
+            sellerApproval = true;
+        } else if (msg.sender == buyer) {
+            buyerApproval = true;
+        }
+        if (sellerApproval && buyerApproval) {
+            eState = EscrowState.serviceApproved;
+            fee();
+            payOutFromEscrow();
+            emit ServicePayment(block.number, address(this).balance); // solhint-disable-line
+        }
+    }
+
+    function cancelEscrow() public checkBlockNumber {
+        if (msg.sender == seller) {
+            sellerCancel = true;
+        } else if (msg.sender == buyer) {
+            buyerCancel = true;
+        }
+        if (sellerCancel && buyerCancel) {
+            eState = EscrowState.escrowCancelled;
+            refund();
+        }
+    }
+
+    function endEscrow() public ifApprovedOrCancelled onlyEscrowOwner {
+        killEscrow();
+    }
+
+    function checkEscrowStatus() public view returns (EscrowState) {
+        return eState;
+    }
+    
+    function getEscrowContractAddress() public view returns (address) {
+        return address(this);
+    }
+    
+    function getAllDeposits() public view returns (uint256[] memory) {
+        return deposits;
+    }
+    
+    function hasBuyerApproved() public view returns (bool) {
+        if (buyerApproval) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function hasSellerApproved() public view returns (bool) {
+        if (sellerApproval) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    function hasBuyerCancelled() public view returns (bool) {
+        if(buyerCancel) {
+            return true;
+        }
+        return false;
+    }
+    
+    function hasSellerCancelled() public view returns (bool) {
+        if(sellerCancel) {
+            return true;
+        }
+        return false;
+    }
+    
+    function getFeeAmount() public view returns (uint256) {
+        return feeAmount;
+    }
+    
+    function getSellermount() public view returns (uint256) {
+        return sellerAmount;
+    }
+    
+    function totalEscrowBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function hasEscrowExpired() public view returns (bool) {
+        if (blockNumber > block.number) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    function getBlockNumber() public view returns (uint256) {
+        return block.number;
+    }
+
+    function killEscrow() internal {
+        selfdestruct(payable(escrowOwner));
+    }
+
+    function payOutFromEscrow() private {
+        balances[buyer] = SafeMath.sub(balances[buyer], address(this).balance);
+        balances[seller] = SafeMath.add(balances[seller], address(this).balance);
+        eState = EscrowState.escrowComplete;
+        sellerAmount = address(this).balance;
+        payable(seller).transfer(address(this).balance);
+    }
+
+    function fee() private {
+        uint totalFee = address(this).balance * (feePercent / 100);
+        feeAmount = totalFee;
+        payable(escrowOwner).transfer(totalFee);
+    }
+
+    function refund() private {
+        payable(buyer).transfer(address(this).balance);
     }
 }
