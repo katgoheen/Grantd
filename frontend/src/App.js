7a8adfa5ae99d93ee8eda889f2e4c38653e0dabe
jengoetz <jennifer.goetz@koho.ca>
2022-06-25 23:46:03 -0400

    add font

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,5 @@
 import "./App.css";
 import { Buffer } from "buffer";
-import { BaseOptionChartStyle } from "./components/dashboard/BaseOptionChartStyle";
-// import { BaseOptionChart } from "./components/dashboard/BaseOptionChart";
-// import { BaseOptionChartStyle } from "./components/dashboard/BaseOptionChartStyle";
-import Dashboard from "./components/dashboard/Dashboard";
 import Form from "./components/form/Form";
 
 function App() {
