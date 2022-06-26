835606686afc36dda78bbc15c5b1a389b1b555d1
jengoetz <jennifer.goetz@koho.ca>
2022-06-25 20:55:24 -0400

    add unlock

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,12 @@ import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/ethereum-provider";
 import { providers } from "ethers";
 import { useState } from "react";
+import { Buffer } from "buffer";
 
 function App() {
   const [address, setAddress] = useState("");
   const [provider, setProvider] = useState();
+  window.Buffer = Buffer;
   const web3Modal = new Web3Modal({
     network: "mainnet",
     cacheProvider: true,
@@ -47,18 +49,25 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h1>Granted</h1>
         <div>{provider ? "Connected!" : "Not connected"}</div>
         {address ? (
           <>
-            <div>Account{address}</div>
+            <div>Account: {address}</div>
             <button onClick={reset}>Disconnect</button>
           </>
         ) : (
           <button onClick={connect}>Connect</button>
         )}
       </header>
+      <button
+        onClick={
+          window.unlockProtocol && window.unlockProtocol.loadCheckoutModal()
+        }
+      >
+        Unlock your grants!
+      </button>
     </div>
   );
 }
