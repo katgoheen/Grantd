5a1a8a4d01c066564ebfb0a7ca9d157db669b490
Turja Chowdhury <42366823+turja-c@users.noreply.github.com>
2022-06-25 23:25:25 -0400

    Updates on conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import { Buffer } from "buffer";
+import { BaseOptionChartStyle } from "./components/dashboard/BaseOptionChartStyle";
+// import { BaseOptionChart } from "./components/dashboard/BaseOptionChart";
+// import { BaseOptionChartStyle } from "./components/dashboard/BaseOptionChartStyle";
 import Dashboard from "./components/dashboard/Dashboard";
+import Form from "./components/form/Form";
 
 function App() {
   window.Buffer = Buffer;
   return (
     <div>
-      <Dashboard />
+      {/* <Dashboard /> */}
+      <Form />
+      {/* <BaseOptionChartStyle /> */}
     </div>
   );
 }
